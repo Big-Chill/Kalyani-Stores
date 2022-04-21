5f1784bb6ee0ca0fc3077e3ca303d2a615727b12
Big-Chill <rohitnandy39@gmail.com>
2022-04-21 22:39:41 +0530

    Version 2

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -12,6 +12,8 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import {Link,useHistory} from 'react-router-dom';
 import {AuthContext} from '../Context/AuthContext';
+import {database} from '../Firebase';
+import { toaster } from 'evergreen-ui';
 
 
 export default function Login() {
@@ -30,7 +32,16 @@ export default function Login() {
 
     try{
       await GoogleLogin();
-      history.push("/form");
+      // history.push("/form");
+      const uid=user.uid;
+      const resp=await database.users.doc(uid).get();
+      if(resp.exists)
+      {
+        history.form("/");
+      }
+      else{
+        history.push("/form");
+      }
     }
     catch(error)
     {
@@ -41,11 +52,24 @@ export default function Login() {
   const emailLogin=async()=>{
     try{
       const resp=await Login(email,password);
+      toaster.success('Sign in successfull',{id: 'forbidden-action',duration:"1"});
       history.push("/");
     }
     catch(error)
     {
-      console.log(error);
+      console.log("Error :- ");
+      if(error.code==="auth/user-not-found")
+      {
+        toaster.warning('You dont have an account with us. Please sign up',{id: 'forbidden-action',duration:"1"});
+      }
+      else if(error.code==="auth/wrong-password")
+      {
+        toaster.warning('Please enter correct password',{id: 'forbidden-action',duration:"1"});
+      }
+      else
+      {
+        toaster.warning('Please enter your email and password to continue',{id: 'forbidden-action'});
+      }
     }
   }
 
@@ -57,23 +81,27 @@ export default function Login() {
   })
 
   return (
-    <Card sx={{ maxWidth: 445, height:"585px", border:5, borderColor:"#fafafa", borderRadius:7, boxShadow: 20, bgcolor:"#edf7fa" }} className="signup-card">
-        <CardContent>
-        <Typography variant="h4" gutterBottom component="div" align="center" sx={{mt:"13.5%", mb:"12%"}}>LOG IN</Typography>
-        <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth={true} margin="dense" size="medium" sx={{mb:"8%"}} value={email} onChange={(e)=>setEmail(e.target.value)}/>
-        <TextField type="password" id="outlined-basic" label="Password" variant="outlined" fullWidth={true} margin="dense" size="medium" sx={{mb:"8%"}} value={password} onChange={(e)=>setPassword(e.target.value)}/>
-        <Typography variant="subtitle2" sx={{ml:"70%", mt:"-6%", mb:"5%"}}>
-          <Link to="/forgot">Forgot password</Link>
-        </Typography>
-        <FormGroup>
-            <Button variant="contained" sx={{borderRadius:10, maxWidth:200, ml:"25%",mb:"6%"}} onClick={emailLogin}>Sign In</Button>
-            <Button variant="contained" fullWidth={true} sx={{mb:"4%"}} onClick={logGoogle}>Google Sign In</Button>
-            <Button variant="contained" fullWidth={true} sx={{mb:"4%"}} onClick={gotoPhone}>LogIn via Phone</Button>
-        </FormGroup>
-        <Typography variant="subtitle2" sx={{ml:"15%"}}>
-              Don't have an account yet?<Link to="/signup">Create Account</Link>
-        </Typography>
-        </CardContent>
-    </Card>
+    <div className="login-div">
+
+        <Card sx={{ maxWidth: 445, height:"585px", border:5, borderColor:"#fafafa", borderRadius:7, boxShadow: 20, bgcolor:"#f2f2f2" }} className="signup-card">
+              <CardContent>
+              <Typography variant="h4" gutterBottom component="div" align="center" sx={{mt:"13.5%", mb:"12%"}}>LOG IN</Typography>
+              <TextField id="outlined-basic" label="Email" variant="outlined" fullWidth={true} margin="dense" size="medium" sx={{mb:"8%"}} value={email} onChange={(e)=>setEmail(e.target.value)}/>
+              <TextField type="password" id="outlined-basic" label="Password" variant="outlined" fullWidth={true} margin="dense" size="medium" sx={{mb:"8%"}} value={password} onChange={(e)=>setPassword(e.target.value)}/>
+              <Typography variant="subtitle2" sx={{ml:"70%", mt:"-6%", mb:"5%"}}>
+                <Link to="/forgot">Forgot password</Link>
+              </Typography>
+              <FormGroup>
+                  <Button variant="contained" sx={{borderRadius:10, maxWidth:200, ml:"25%",mb:"6%"}} onClick={emailLogin}>Sign In</Button>
+                  <Button variant="contained" fullWidth={true} sx={{mb:"4%"}} onClick={logGoogle}>Google Sign In</Button>
+                  <Button variant="contained" fullWidth={true} sx={{mb:"4%"}} onClick={gotoPhone}>LogIn via Phone</Button>
+              </FormGroup>
+              <Typography variant="subtitle2" sx={{ml:"15%"}}>
+                    Don't have an account yet?<Link to="/signup">Create Account</Link>
+              </Typography>
+              </CardContent>
+          </Card>
+
+    </div>
   );
 }
--- src/Components/Prodlist.js
+++ src/Components/Prodlist.js
@@ -2,9 +2,34 @@ import React,{useState,useEffect,useContext} from 'react';
 import Header from './Header';
 import Prodcard from './Prodcard';
 import axios from 'axios';
+import {database} from '../Firebase';
+import {AuthContext} from '../Context/AuthContext';
+import {Link,useHistory} from 'react-router-dom';
+
 
 export default function Prodlist() {
   const[items,setItems]=useState([]);
+  const[filtereddata,setFilteredData]=useState([]);
+  const[searchinput,setSearchInput]=useState("");
+  const{user,Logout}=useContext(AuthContext);
+  const history=useHistory();
+
+  
+  function handleSearch(searchval)
+    {
+        const text=searchval.toLowerCase();
+        setSearchInput(text);
+        console.log(text);
+        const newarr=items.filter(item=>item.name.toLowerCase().includes(text));
+        if(text!="")
+        {
+            setFilteredData(newarr);
+        }
+        else
+        {
+            setFilteredData(items);
+        }
+    }
 
   useEffect(()=>{
     async function fetchItems()
@@ -21,10 +46,27 @@ export default function Prodlist() {
   return (
     <div>
         <Header/>
+        <div className="search-div">
+                        
+                        <div className="opt-card2">
+                            <div className="input-group mb-3">
+                                <input type="text" className="form-control" placeholder="Type to Search..." aria-label="Username" aria-describedby="basic-addon1" style={{marginTop:"-60px", fontSize:"25px"}} onChange={(e)=>handleSearch(e.target.value)}/>
+                            </div>
+                        </div> 
+                        
+                    </div>
         <div className="main-body">
-          {items.map((obj,idx)=>(
-            <Prodcard item={obj}/>
-          ))}
+          {
+            searchinput.length==0?
+
+            items.map((obj,idx)=>(
+              <Prodcard item={obj}/>
+            ))
+            :
+            filtereddata.map((obj,idx)=>(
+              <Prodcard item={obj}/>
+            ))
+          }
         </div>
     </div>
   )
